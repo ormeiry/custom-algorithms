7e690fce05e3e0fa455137b84d671467b44199b2
ormeiry <ormeiry2411@gmail.com>
2021-03-24 11:07:53 +0200

    Added a new algorithm.

---
+++ arrayContains.js
@@ -0,0 +1,21 @@
+// Given two arrays, check if they have a common item.
+// If they do, return true, else return false.
+
+function commonArrItem(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) return 'Invalid Input';
+  if (!arr1.length || !arr1.length) return false;
+
+  const itemsMap = {};
+
+  arr1.forEach((item) => {
+    itemsMap[item] = true;
+  });
+
+  for (let item of arr2) {
+    if (itemsMap[item]) return true;
+  }
+  return false;
+}
+
+const res = commonArrItem([1, 2, 3], [0, 3, 5, 6, 8]);
+console.log(res); // Output: true
